23204269030380649e0e373c9355b83c2a6284ba
ser-salt <84048267+ser-salt@users.noreply.github.com>
2021-08-14 03:27:35 -0400

    created scorecard class
    dm if anything is unclear

---
+++ YahtzeeGame/src/yahtzeegame/ScoreCard.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package yahtzeegame;
+
+/**
+ *
+ * @author Brendan
+ */
+public class ScoreCard {
+    private int totalScore;
+    private boolean isComplete;
+   
+    public ScoreCard() {
+        totalScore = 0;
+        isComplete = false;
+        yahtzeeBonus = false;
+    }
+    
+    while(isComplete){
+}
+    public void (){
+    }
+}
