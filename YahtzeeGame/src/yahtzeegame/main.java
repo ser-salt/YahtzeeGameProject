2da3d8a5d7ceefb95497285b36cddd0a3848c7fc
navkiranksingh <84054141+navkiranksingh@users.noreply.github.com>
2021-08-14 14:05:36 -1000

    Create main.java

---
+++ YahtzeeGame/src/yahtzeegame/main.java
@@ -0,0 +1,19 @@
+    public static void main(String args[])
+
+     {
+
+          //Instantiate an instance of class Game
+
+          Game game = new Game();
+
+          //Call method displayPlayers() in class Game
+
+          game.displayPlayers();
+
+          //Call method playGame() in class Game
+
+          game.playGame();
+
+     }
+
+}
